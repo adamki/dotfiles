163d682dc33cf608913936bfa98069d260dca422
adamki <adajensen@gmail.com>
2018-11-17 16:18:42 -0800

    removes installation of oh-my-zsh as it is handled by antigen in .zshrc

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -23,6 +23,7 @@ files="tmux.conf
 vimrc
 zshrc
 config/nvim/init.vim
+config/nvim/ftplugin/plugins.vim
 config/nvim/ftplugin/nerdtree.vim
 config/nvim/ftplugin/system.vim
 config/nvim/ftplugin/keymappings.vim
@@ -66,10 +67,6 @@ printf "Complete"
 install_zsh () {
 # Test to see if zshell is installed.  If it is:
 if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
-  # Clone my oh-my-zsh repository from GitHub only if it isn't already present
-  if [[ ! -d $dir/oh-my-zsh/ ]]; then
-    git clone http://github.com/robbyrussell/oh-my-zsh.git
-  fi
   # Set the default shell to zsh if it isn't currently set to zsh
   if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
     chsh -s $(which zsh)
