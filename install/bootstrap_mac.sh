6dec2fad9481a2e2a9f0e6b67fd34ec9a2b5b402
adamki <adajensen@gmail.com>
2019-10-03 21:24:15 -0700

    creates install dir. improves scripting and terminal output

--- install/bootstrap_mac.sh
+++ install/bootstrap_mac.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
-echo "Starting bootstrapping"
-echo "Checking for Brew..."
-# install Brew
+. "${DOTFILES}/utils/colors.sh"
+
+echo -e "${yellow}Check if Brew is installed${reset}"
 if test ! $(which brew); then
-  echo "Installing homebrew..."
+  echo -e "${yellow}Installing Brew...${reset}"
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
-echo "Upating Brew..."
-# Update homebrew recipes
+
+echo -e "${yellow}Brew Found!\nUpating Brew...${reset}"
 brew update
 
 TAPS=(
   homebrew/cask-fonts
   koekeishiya/formulae
 )
-echo 'Tapping Homebrew...'
+echo -e "\n${yellow}Fetching Brew Taps...${reset}"
 brew tap ${TAPS[@]}
 
 PACKAGES=(
   zsh
   antigen
   neofetch
-  lolcat
   ripgrep
   fzf
   tmux
@@ -33,63 +32,63 @@ PACKAGES=(
   yabai
   skhd
 )
-echo "Installing packages..."
+echo -e "${HR}Installing Brew Packages...${HR}"
 brew install ${PACKAGES[@]}
 
 CASKS=(
   firefox
   slack
 )
-echo "Installing cask apps..."
+echo -e "${HR}Installing Brew Casks...${HR}"
 brew cask install ${CASKS[@]}
 
 FONTS=(
   font-fira-code
 )
-echo "Installing Fonts..."
+echo -e "${HR}Installing Fonts...${HR}"
 brew cask install ${FONTS[@]}
 
-echo "Cleaning up..."
+echo -e "${HR}Cleaning up...${HR}"
 brew cleanup
 
 GEMS=(
+  lolcat
   bundler
   neovim
 )
-echo "Install Gems..."
+echo -e "${HR}Installing Gems...${HR}"
 gem install ${GEMS[@]}
-
 
 GLOBAL_NPM=(
   neovim
   livedown
 )
 
-echo 'Installing NPM Packages...'
+echo -e "${HR}Installing GLOBAL NPM Packages......${HR}"
 npm install -g ${GLOBAL_NPM[@]}
 
 # MISC INSTALLS
-echo "Installing Kitty Terminal..."
+echo -e "${HR}Installing Kitty Terminal...${HR}"
 curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
-echo "Installing TPM..."
+echo -e "${HR}Installing TPM...${HR}"
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-echo "Installing NVM..."
+echo -e "${HR}Installing NVM...${HR}"
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | zsh
-echo "Checking NVM Version..."
+echo -e "${HR}Checking NVM version...${HR}"
 source ${HOME}/.nvm/nvm.sh
 nvm --version
-echo "Installing Node..."
-nvm install node
-echo "Checking rbenv installation..."
-curl -fsSL https://github.com/rbenv/rbenv-installer/raw/master/bin/rbenv-doctor | bash
-echo "installing Neovim..."
+echo -e "${HR}Installing Latest Node...${HR}"
+nvm install node #"node is alway latest version"
+echo -e "${HR}Check rbenv installation...${HR}"
+curl -fsSL https://github.com/rbenv/rbenv-installer/raw/master/bin/rbenv-doctor | zsh
+echo -e "${HR}Installing Neovim...${HR}"
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-echo "installing Neovim providers..."
+echo -e "${HR}Installing Neovim Python Providers...${HR}"
 python2 -m pip install --user --upgrade pynvim
 python3 -m pip install --user --upgrade pynvim
-echo "Settings up FZF keybindings..."
+echo -e "${HR}Set up FZF keymappings...${HR}"
 /usr/local/opt/fzf/install
-echo 'Setting ZSH as default!'
+echo -e "${HR}Set default shell to Zsh...${HR}"
 chsh -s $(which zsh)
-echo 'Bootstrap Complete! \n You should now restart your shell.'
+echo -e "${HR}\nBootstrap Complete!\n${HR}"
---
+++ install/install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Setup PATHS
+INSTALL_DIR="${DOTFILES}/install"
+
+BOOTSTRAP_MAC="${INSTALL_DIR}/bootstrap_mac.sh"
+SYMLINK_MAC="${INSTALL_DIR}/makesymlinks_mac.sh"
+
+LINUX_INSTALL="${INSTALL_DIR}/bootstrap_linux.sh"
+LINUX_SYMLINKS="${INSTALL_DIR}/makesymlinks_linux.sh"
+
+. "${DOTFILES}/utils/colors.sh"
+
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+  echo "coming soon"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  echo -e "${HR}Starting Bootstrap Script${HR}"
+  . "$BOOTSTRAP_MAC"
+  # . "$SYMLINK_MAC"
+fi
