5e3268da940f0a649df338dc9a2607ee67fecc8e
adam ki <adajensen@gmail.com>
2020-02-16 13:08:34 -0800

    wip

--- install/bootstrap_linux.sh
+++ install/bootstrap_linux.sh
@@ -23,8 +23,8 @@ PACKAGES=(
   curl
   bat
   xcape
+  xclip
   ripgrep
-  tmux
   ranger
   htop
   firefox
@@ -61,44 +61,47 @@ GLOBAL_NPM_PACKAGES=(
   eslint
 )
 
-echo -e "${HR}Installing Gems...${HR}"
-sudo gem install ${GEMS[@]}
+echo -e "${hr}Cloning pfetch...${hr}"
+git clone https://github.com/dylanaraps/pfetch.git ~/pfetch
 
-echo -e "${hr}Cloning Pfetch...${hr}"
-git clone git@github.com:dylanaraps/pfetch.git ~/pfetch
 echo -e "${hr}Installing Plug...${hr}"
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 echo -e "${hr}installing kitty terminal...${hr}"
 curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
-echo -e "${HR}Installing TPM...${HR}"
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
 echo "Installing Antigen..."
 curl -L git.io/antigen > ~/antigen.zsh
+
 echo -e "${HR}Installing NVM...${HR}"
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | zsh
+
 echo -e "${HR}Checking NVM version...${HR}"
 source ${HOME}/.nvm/nvm.sh
 nvm --version
+
 echo -e "${HR}Installing Latest Node...${HR}"
 nvm install node #"node is alway latest version"
+
 echo -e "${HR}Installing NPM Packages...${HR}"
 npm i -g ${GLOBAL_NPM_PACKAGES[@]}
 
 curl -fsSL https://github.com/rbenv/rbenv-installer/raw/master/bin/rbenv-installer | bash
 echo -e "${HR}Installing latest ruby and setting as system default...${HR}"
+
 rbenv install $(rbenv install -l | grep -v - | tail -1)
 rbenv global $(rbenv install -l | grep -v - | tail -1)
-echo -e "${HR}Installing RUBY Gems...${HR}"
-gem install $GEMS
 echo -e "${HR}Check rbenv installation...${HR}"
 curl -fsSL https://github.com/rbenv/rbenv-installer/raw/master/bin/rbenv-doctor | bash
 
-
+echo -e "${HR}Installing Gems...${HR}"
+sudo gem install ${GEMS[@]}
 
 echo -e "${HR}Installing Neovim Python Providers...${HR}"
 python2 -m pip install --user --upgrade pynvim
 python3 -m pip install --user --upgrade pynvim
+
 echo -e "${HR}Cloning FZF...${HR}"
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
