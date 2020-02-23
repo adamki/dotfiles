b28a18e23ef814a38b00a7063c8e378b391038f1
adam ki <adajensen@gmail.com>
2020-02-22 16:12:05 -0800

    restoring bootstrap_linux to 0d5d5d7

--- install/bootstrap_linux.sh
+++ install/bootstrap_linux.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 . ./utils/colors.sh
+# install
 
-AUR_REPOS=(
+AUR_REPOS="
   gotop
   neovim-nightly
-)
+"
 
-PACMAN_PACKAGES=(
+PAMAC_PACKAGES=(
   feh
   lolcat
+)
+
+PACKAGES=(
   bat
   compton
   curl
@@ -28,7 +32,7 @@ PACMAN_PACKAGES=(
   zsh
 )
 
-RUBY_GEMS=(
+GEMS=(
   bundler
   neovim
 )
@@ -36,6 +40,7 @@ RUBY_GEMS=(
 GLOBAL_NPM_PACKAGES=(
   bash-language-server
   eslint
+  livedown
   neovim
   vim-language-server
 )
@@ -43,6 +48,28 @@ GLOBAL_NPM_PACKAGES=(
 echo -e "${HR}"
 echo -e "${bold}Looking for fastest Mirrors...${normal}"
 sudo pacman-mirrors --fasttrack
+
+echo -e "${HR}"
+echo -e "${bold}Updating Pacman...${normal}"
+sudo pacman -Syyu
+
+echo -e "${HR}"
+echo -e "${bold}Cloning && Building AUR Repos...${normal}"
+pamac clone $AUR_REPOS
+pamac build $AUR_REPOS
+
+
+echo -e "${HR}"
+echo -e "${bold}Installing PAMAC Packages...${normal}"
+pamac install ${PAMAC_PACKAGES[@]}
+
+echo -e "${HR}"
+echo -e "${bold}Installing PACMAN Packages...${normal}"
+sudo pacman -S ${PACKAGES[@]}
+
+echo -e "${HR}"
+echo -e "${bold}Installing Gems...${normal}"
+sudo gem install ${GEMS[@]}
 
 echo -e "${HR}"
 echo -e "${bold}Cloning pFetch...${normal}"
@@ -55,8 +82,7 @@ curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
 
 echo -e "${HR}"
 echo -e "${bold}Installing Kitty Terminal...${normal}"
-curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin \
-  launch=n
+curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
 
 echo -e "${HR}"
 echo -e "${bold}Installing Antigen...${normal}"
@@ -64,7 +90,7 @@ curl -L git.io/antigen > ~/antigen.zsh
 
 echo -e "${HR}"
 echo -e "${bold}Installing NVM...${normal}"
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | zsh
 
 echo -e "${HR}"
 echo -e "${bold}Sourcing NVM...${normal}"
@@ -72,69 +98,47 @@ source ${HOME}/.nvm/nvm.sh
 nvm --version
 
 echo -e "${HR}"
-echo -e "${bold}Installing Rbenv...${normal}"
-git clone https://github.com/rbenv/rbenv.git ~/.rbenv
-~/.rbenv/bin/rbenv init
+echo -e "${bold}Installing Latest Node...${normal}"
+nvm install node #"node is alway latest version"
 
 echo -e "${HR}"
-echo -e "${bold}Restarting Shell...${normal}"
-exec zsh
+echo -e "${bold}Installing NPM Packages...${normal}"
+npm i -g ${GLOBAL_NPM_PACKAGES[@]}
 
 echo -e "${HR}"
-echo -e "${bold}Installing Ruby Build...${normal}"
-mkdir -p "$(rbenv root)"/plugins
-git clone https://github.com/rbenv/ruby-build.git "$(rbenv root)"/plugins/ruby-build
+echo -e "${bold}RBENV doctor...${normal}"
+curl -fsSL https://github.com/rbenv/rbenv-installer/raw/master/bin/rbenv-installer | bash
+
+echo -e "${HR}"
+echo -e "${bold}Installing Latest Ruby...${normal}"
+rbenv install $(rbenv install -l | grep -v - | tail -1)
+
+echo -e "${HR}"
+echo -e "${bold}Setting Ruby Default...${normal}"
+rbenv global $(rbenv install -l | grep -v - | tail -1)
+
+echo -e "${HR}"
+echo -e "${bold}Installing Ruby Gesm...${normal}"
+gem install ${GEMS[@]}
 
 echo -e "${HR}"
 echo -e "${bold}RBENV doctor...${normal}"
 curl -fsSL https://github.com/rbenv/rbenv-installer/raw/master/bin/rbenv-doctor | bash
 
-# echo -e "${HR}"
-# echo -e "${bold}Installing Latest Node...${normal}"
-# nvm install node #"node is alway latest version"
-#
-# echo -e "${HR}"
-# echo -e "${bold}Installing NPM Packages...${normal}"
-# npm i -g ${GLOBAL_NPM_PACKAGES[@]}
+echo -e "${HR}Installing Neovim Python Providers...${HR}"
+echo -e "${HR}"
+echo -e "${bold}Installing NeoVIM Python Provider...${normal}"
+python2 -m pip install --user --upgrade pynvim
+python3 -m pip install --user --upgrade pynvim
 
-# echo -e "${HR}"
-# echo -e "${bold}Installing Latest Ruby...${normal}"
-# rbenv install $(rbenv install -l | grep -v - | tail -1)
-#
-# echo -e "${HR}"
-# echo -e "${bold}Setting Ruby Default...${normal}"
-# rbenv global $(rbenv install -l | grep -v - | tail -1)
-#
-# echo -e "${HR}"
-# echo -e "${bold}Installing Ruby Gesm...${normal}"
-# gem install ${RUBY_GEMS[@]}
-#
-# echo -e "${HR}"
-# echo -e "${bold}Installing NeoVIM Python Provider...${normal}"
-# python2 -m pip install --user --upgrade pynvim
-# python3 -m pip install --user --upgrade pynvim
-#
-# echo -e "${HR}"
-# echo -e "${bold}Installing FZF...${normal}"
-# git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-# ~/.fzf/install
-#
-# echo -e "${HR}"
-# echo -e "${bold}Updating Pacman...${normal}"
-# sudo pacman -Syyu
-#
-# echo -e "${HR}"
-# echo -e "${bold}Cloning && Building AUR Repos...${normal}"
-# pamac clone ${AUR_REPOS[@]}
-# pamac build ${AUR_REPOS[@]}
-#
-# echo -e "${HR}"
-# echo -e "${bold}Installing PACMAN Packages...${normal}"
-# sudo pacman -S ${PACMAN_PACKAGES[@]}
-#
-# echo -e "${HR}"
-# echo -e "${bold}Changing default SHELL to FZF...${normal}"
-# chsh -s $(which zsh)
-#
-# echo -e "${HR}"
-# echo -e "${bold}${green}Bootstrap Complete...${normal}"
+echo -e "${HR}"
+echo -e "${bold}Installing FZF...${normal}"
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+
+echo -e "${HR}"
+echo -e "${bold}Changing default SHELL to FZF...${normal}"
+chsh -s $(which zsh)
+
+echo -e "${HR}"
+echo -e "${bold}${green}Bootstrap Complete...${normal}"
