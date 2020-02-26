0a95b86b8a28beae7aa555e4b68e78a7c759ac2a
adamki <adajensen@gmail.com>
2020-02-26 10:57:34 -0800

    restores install script after testing os logic conditionals

--- install/install.sh
+++ install/install.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
-  # . ./install/linux_variables.sh
-  # . ./install/symlink.sh
-  if [ -f /etc/debian_version ]; then
-    echo "DEBIan"
-    # . ./install/debian_bootstrapper.sh
+  . ./install/linux_variables.sh
+  . ./install/symlink.sh
+  if [ -f "/etc/debian_version" ]; then
+    . ./install/debian_bootstrapper.sh
   elif [ -f "/etc/arch-release" ]; then
-    echo "ARCH"
-    # . ./install/arch_bootstrapper.sh
+    . ./install/arch_bootstrapper.sh
   elif [ -f "/etc/fedora-release" ]; then
-    echo "FEDORA"
-    # . ./install/fedora_bootstrapper.sh
+    . ./install/fedora_bootstrapper.sh
   fi
 elif [[ "$OSTYPE" == "darwin"* ]]; then
   echo "MAC"
-  # . ./install/macos_variables.sh
-  # . ./install/symlink.sh
-  # . ./install/macos_bootstrapper.sh
+  . ./install/macos_variables.sh
+  . ./install/symlink.sh
+  . ./install/macos_bootstrapper.sh
 fi
