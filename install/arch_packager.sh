91145054c249959459cd5edca4d48043431a0628
adam ki <adajensen@gmail.com>
2020-02-25 16:08:26 -0800

    troubshooting

--- install/arch_packager.sh
+++ install/arch_packager.sh
@@ -42,22 +42,6 @@ GLOBAL_NPM_PACKAGES=(
 )
 
 echo -e "${HR}"
-echo -e "${bold}Installing Latest Ruby...${normal}"
-rbenv install $(rbenv install -l | grep -v - | tail -1)
-
-echo -e "${HR}"
-echo -e "${bold}Setting Ruby Default...${normal}"
-rbenv global $(rbenv install -l | grep -v - | tail -1)
-
-echo -e "${HR}"
-echo -e "${bold}Installing Ruby Gems...${normal}"
-gem install ${RUBY_GEMS[@]}
-
-echo -e "${HR}"
-echo -e "${bold}RBENV doctor...${normal}"
-curl -fsSL https://github.com/rbenv/rbenv-installer/raw/master/bin/rbenv-doctor | bash
-
-echo -e "${HR}"
 echo -e "${bold}Installing NPM Packages...${normal}"
 # npm i -g ${GLOBAL_NPM_PACKAGES[@]}
 npm i -g bash-language-server
@@ -72,16 +56,31 @@ python3 -m pip install --user --upgrade pynvim
 
 echo -e "${HR}"
 echo -e "${bold}Cloning && Building AUR Repos...${normal}"
-pamac clone ${AUR_REPOS[@]}
-pamac build ${AUR_REPOS[@]}
+pamac build --no-confirm ${AUR_REPOS[@]}
+
+echo -e "${HR}"
+echo -e "${bold}Installing PAMAC Packages...${normal}"
+pamac install ${PAMAC_PACKAGES[@]}
 
 echo -e "${HR}"
 echo -e "${bold}Installing PACMAN Packages...${normal}"
 sudo pacman -S ${PACMAN_PACKAGES[@]}
 
 echo -e "${HR}"
-echo -e "${bold}Installing PAMAC Packages...${normal}"
-pamac install ${PAMAC_PACKAGES[@]}
+echo -e "${bold}Installing Latest Ruby...${normal}"
+rbenv install $(rbenv install -l | grep -v - | tail -1)
+
+echo -e "${HR}"
+echo -e "${bold}Setting Ruby Default...${normal}"
+rbenv global $(rbenv install -l | grep -v - | tail -1)
+
+echo -e "${HR}"
+echo -e "${bold}Installing Ruby Gems...${normal}"
+gem install ${RUBY_GEMS[@]}
+
+echo -e "${HR}"
+echo -e "${bold}RBENV doctor...${normal}"
+curl -fsSL https://github.com/rbenv/rbenv-installer/raw/master/bin/rbenv-doctor | bash
 
 echo -e "${HR}"
 echo -e "${bold}${green}Packager Complete...${normal}"
