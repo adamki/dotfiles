49e36184a5d875cf58f05b0fbfebd82487b4b8ac
Adam Jensen <adam.jensen@peerfit.com>
2020-05-07 12:51:06 -0700

    fixes spacing

--- keyboardio/keyboardio.ino
+++ keyboardio/keyboardio.ino
@@ -253,7 +253,7 @@ KEYMAPS(
    ___, ___, ___, ___,
    ___,
 
-   ___,  ___, Key_7, Key_8,      Key_9,              Key_KeypadSubtract, M(MACRO_VERSION_INFO),
+   ___,                    ___, Key_7, Key_8,      Key_9,              Key_KeypadSubtract, M(MACRO_VERSION_INFO),
    ___,                    ___, Key_4, Key_5,      Key_6,              Key_KeypadAdd,      ___,
                            ___, Key_1, Key_2,      Key_3,              Key_Equals,         ___,
    ___,                    ___, Key_0, Key_Period, Key_KeypadMultiply, Key_KeypadDivide,   Key_Enter,
