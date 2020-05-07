48550a420ee8f58bb9969e5028fa7e11e59a3077
Adam Jensen <adam.jensen@peerfit.com>
2020-05-07 13:29:55 -0700

    comment out dvorak and colemak layouts cuz im not intersted

--- keyboardio/keyboardio.ino
+++ keyboardio/keyboardio.ino
@@ -187,56 +187,56 @@ KEYMAPS(
    Key_RightShift,        Key_LeftAlt,   Key_Enter,    Key_Spacebar,
    ShiftToLayer(FUNCTION)),
 
-#elif defined (PRIMARY_KEYMAP_DVORAK)
-
-  [PRIMARY] = KEYMAP_STACKED
-  (___,          Key_1,         Key_2,     Key_3,      Key_4, Key_5, Key_LEDEffectNext,
-   Key_Backtick, Key_Quote,     Key_Comma, Key_Period, Key_P, Key_Y, Key_Tab,
-   Key_PageUp,   Key_A,         Key_O,     Key_E,      Key_U, Key_I,
-   Key_PageDown, Key_Semicolon, Key_Q,     Key_J,      Key_K, Key_X, Key_Escape,
-   Key_LeftControl, Key_Backspace, Key_LeftGui, Key_LeftShift,
-   ShiftToLayer(FUNCTION),
-
-   M(MACRO_ANY),   Key_6, Key_7, Key_8, Key_9, Key_0, LockLayer(NUMPAD),
-   Key_Enter,      Key_F, Key_G, Key_C, Key_R, Key_L, Key_Slash,
-                   Key_D, Key_H, Key_T, Key_N, Key_S, Key_Minus,
-   Key_RightAlt,   Key_B, Key_M, Key_W, Key_V, Key_Z, Key_Equals,
-   Key_RightShift, Key_LeftAlt, Key_Spacebar, Key_RightControl,
-   ShiftToLayer(FUNCTION)),
-
-#elif defined (PRIMARY_KEYMAP_COLEMAK)
-
-  [PRIMARY] = KEYMAP_STACKED
-  (___,          Key_1, Key_2, Key_3, Key_4, Key_5, Key_LEDEffectNext,
-   Key_Backtick, Key_Q, Key_W, Key_F, Key_P, Key_G, Key_Tab,
-   Key_PageUp,   Key_A, Key_R, Key_S, Key_T, Key_D,
-   Key_PageDown, Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Escape,
-   Key_LeftControl, Key_Backspace, Key_LeftGui, Key_LeftShift,
-   ShiftToLayer(FUNCTION),
-
-   M(MACRO_ANY),  Key_6, Key_7, Key_8,     Key_9,         Key_0,         LockLayer(NUMPAD),
-   Key_Enter,     Key_J, Key_L, Key_U,     Key_Y,         Key_Semicolon, Key_Equals,
-                  Key_H, Key_N, Key_E,     Key_I,         Key_O,         Key_Quote,
-   Key_RightAlt,  Key_K, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_Minus,
-   Key_RightShift, Key_LeftAlt, Key_Spacebar, Key_RightControl,
-   ShiftToLayer(FUNCTION)),
-
-#elif defined (PRIMARY_KEYMAP_CUSTOM)
-  // Edit this keymap to make a custom layout
-  [PRIMARY] = KEYMAP_STACKED
-  (___,          Key_1, Key_2, Key_3, Key_4, Key_5, Key_LEDEffectNext,
-   Key_Backtick, Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,
-   Key_PageUp,   Key_A, Key_S, Key_D, Key_F, Key_G,
-   Key_PageDown, Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Escape,
-   Key_LeftControl, Key_Backspace, Key_LeftGui, Key_LeftShift,
-   ShiftToLayer(FUNCTION),
-
-   M(MACRO_ANY),  Key_6, Key_7, Key_8,     Key_9,         Key_0,         LockLayer(NUMPAD),
-   Key_Enter,     Key_Y, Key_U, Key_I,     Key_O,         Key_P,         Key_Equals,
-                  Key_H, Key_J, Key_K,     Key_L,         Key_Semicolon, Key_Quote,
-   Key_RightAlt,  Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_Minus,
-   Key_RightShift, Key_LeftAlt, Key_Spacebar, Key_RightControl,
-   ShiftToLayer(FUNCTION)),
+// #elif defined (PRIMARY_KEYMAP_DVORAK)
+//
+//   [PRIMARY] = KEYMAP_STACKED
+//   (___,          Key_1,         Key_2,     Key_3,      Key_4, Key_5, Key_LEDEffectNext,
+//    Key_Backtick, Key_Quote,     Key_Comma, Key_Period, Key_P, Key_Y, Key_Tab,
+//    Key_PageUp,   Key_A,         Key_O,     Key_E,      Key_U, Key_I,
+//    Key_PageDown, Key_Semicolon, Key_Q,     Key_J,      Key_K, Key_X, Key_Escape,
+//    Key_LeftControl, Key_Backspace, Key_LeftGui, Key_LeftShift,
+//    ShiftToLayer(FUNCTION),
+//
+//    M(MACRO_ANY),   Key_6, Key_7, Key_8, Key_9, Key_0, LockLayer(NUMPAD),
+//    Key_Enter,      Key_F, Key_G, Key_C, Key_R, Key_L, Key_Slash,
+//                    Key_D, Key_H, Key_T, Key_N, Key_S, Key_Minus,
+//    Key_RightAlt,   Key_B, Key_M, Key_W, Key_V, Key_Z, Key_Equals,
+//    Key_RightShift, Key_LeftAlt, Key_Spacebar, Key_RightControl,
+//    ShiftToLayer(FUNCTION)),
+//
+// #elif defined (PRIMARY_KEYMAP_COLEMAK)
+//
+//   [PRIMARY] = KEYMAP_STACKED
+//   (___,          Key_1, Key_2, Key_3, Key_4, Key_5, Key_LEDEffectNext,
+//    Key_Backtick, Key_Q, Key_W, Key_F, Key_P, Key_G, Key_Tab,
+//    Key_PageUp,   Key_A, Key_R, Key_S, Key_T, Key_D,
+//    Key_PageDown, Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Escape,
+//    Key_LeftControl, Key_Backspace, Key_LeftGui, Key_LeftShift,
+//    ShiftToLayer(FUNCTION),
+//
+//    M(MACRO_ANY),  Key_6, Key_7, Key_8,     Key_9,         Key_0,         LockLayer(NUMPAD),
+//    Key_Enter,     Key_J, Key_L, Key_U,     Key_Y,         Key_Semicolon, Key_Equals,
+//                   Key_H, Key_N, Key_E,     Key_I,         Key_O,         Key_Quote,
+//    Key_RightAlt,  Key_K, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_Minus,
+//    Key_RightShift, Key_LeftAlt, Key_Spacebar, Key_RightControl,
+//    ShiftToLayer(FUNCTION)),
+//
+// #elif defined (PRIMARY_KEYMAP_CUSTOM)
+//   // Edit this keymap to make a custom layout
+//   [PRIMARY] = KEYMAP_STACKED
+//   (___,          Key_1, Key_2, Key_3, Key_4, Key_5, Key_LEDEffectNext,
+//    Key_Backtick, Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,
+//    Key_PageUp,   Key_A, Key_S, Key_D, Key_F, Key_G,
+//    Key_PageDown, Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Escape,
+//    Key_LeftControl, Key_Backspace, Key_LeftGui, Key_LeftShift,
+//    ShiftToLayer(FUNCTION),
+//
+//    M(MACRO_ANY),  Key_6, Key_7, Key_8,     Key_9,         Key_0,         LockLayer(NUMPAD),
+//    Key_Enter,     Key_Y, Key_U, Key_I,     Key_O,         Key_P,         Key_Equals,
+//                   Key_H, Key_J, Key_K,     Key_L,         Key_Semicolon, Key_Quote,
+//    Key_RightAlt,  Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_Minus,
+//    Key_RightShift, Key_LeftAlt, Key_Spacebar, Key_RightControl,
+//    ShiftToLayer(FUNCTION)),
 
 #else
 
