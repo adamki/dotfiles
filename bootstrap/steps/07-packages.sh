23347f1330c0cea73d7703eab67fed77b529700d
Adam <adam-jensen@Adams-MacBook-Pro.local>
2026-01-29 17:39:39 -0800

    fix commands file arg for xargs

--- bootstrap/steps/07-packages.sh
+++ bootstrap/steps/07-packages.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+
 source "$(dirname "$0")/../lib/brew.sh"
 source "$(dirname "$0")/../lib/nvm.sh"
 source "$(dirname "$0")/../lib/rbenv.sh"
@@ -10,8 +12,8 @@ ensure_nvm
 ensure_rbenv
 ensure_pyenv
 
-xargs brew install <manifests/brew.txt
-xargs brew install --cask <manifests/casks.txt
-xargs npm install -g <manifests/npm.txt
-xargs gem install --no-document <manifests/gems.txt
-xargs luarocks install <manifests/luarocks.txt
+xargs -n 1 brew install <"$ROOT_DIR/manifests/brew.txt"
+xargs -n 1 brew install --cask <"$ROOT_DIR/manifests/casks.txt"
+xargs -n 1 npm install -g <"$ROOT_DIR/manifests/npm.txt"
+xargs -n 1 gem install <"$ROOT_DIR/manifests/gems.txt"
+xargs -n 1 luaroocks <"$ROOT_DIR/manifests/luarocks.txt"
